e8750aba719e2862ce2ad8283cd9841353fc740a
James Macindoe <james.macindoe@gmail.com>
2020-03-22 12:06:07 +1100

    Organize AppDelegate and SceneDelegate

---
+++ Netwerk/System/SceneDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  SceneDelegate.swift
+//  Netwerk
+//
+//  Created by James Macindoe on 22/3/20.
+//  Copyright © 2020 James Macindoe. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let contentView = ContentView().environment(\.managedObjectContext, managedObjectContext)
+
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
